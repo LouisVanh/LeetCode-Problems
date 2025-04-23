971593fc496206e1baefaa2dcbcd828d11eeaa2b
Louis Vanhove <vanhovelouis123@gmail.com>
2025-04-23 21:47:29 +0200

    Climbing stairs problem with DP, bottoms up and SO

--- 70_climbing_stairs.py
+++ 70_climbing_stairs.py
@@ -1,19 +1,49 @@
 class Solution:
-    global lookup_table
-    lookup_table = [0,1,2,3,5,8]
-    def climbStairs(self, n: int) -> int:
-        if(n <= 3): return n
-        if(len(lookup_table) > n):
-            print(f"found in lookup_table with length {len(lookup_table)}: {n}")
-            return lookup_table[n]
-        else:
-            if(n == len(lookup_table)):
-                print("please append")
-                print(lookup_table)
-                lookup_table.append(lookup_table[-1] + lookup_table[-2])
-                print(lookup_table)
+    #Recursion:
+    # global lookup_table
+    # lookup_table = [0,1,2,3,5,8]
+    # def climbStairs(self, n: int) -> int:
+    #     if(n <= 3): return n
+    #     if(len(lookup_table) > n):
+    #         print(f"found in lookup_table with length {len(lookup_table)}: {n}")
+    #         return lookup_table[n]
+    #     else:
+    #         if(n == len(lookup_table)):
+    #             print("please append")
+    #             print(lookup_table)
+    #             lookup_table.append(lookup_table[-1] + lookup_table[-2])
+    #             print(lookup_table)
 
-            return self.climbStairs( n-1) + self.climbStairs(n-2)
-        
+    #         return self.climbStairs( n-1) + self.climbStairs(n-2)
+
+
+    # Bottoms up DP
+    # def climbStairs(self, n: int) -> int:
+    #     dp=[-1]*(n+1)
+    #     dp[0] = 1
+    #     dp[1] = 1
+
+    #     for i in range(2,n+1):
+    #         dp[i] = dp[i-1] + dp[i-2]
+    #     print(dp)
+    #     return dp[n]
+
+    # DP: space optimised bottoms up
+    def climbStairs(self, n: int) -> int:
+        if(n<=1): return 1
+        prev2 = 1
+        prev = 1
+        curr = 1
+
+        for _ in range(2,n+1):
+            # tmp = prev2
+            prev2 = prev
+            prev = curr
+            curr = prev+prev2
+            print(curr, " is the curr value")
+        return curr
+
 sol = Solution()
 print(sol.climbStairs(40))
+print(sol.climbStairs(1))
+print(sol.climbStairs(2))
