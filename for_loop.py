a0b59e38d09280682dc8c176eeb68ad6066edffe
LouisVanhove <vanhovelouis123@gmail.com>
2025-06-01 08:50:05 +0200

    basic testing for loop

--- for_loop.py
+++ for_loop.py
@@ -1,7 +1,13 @@
 # i = 1
-for i in range(1, 4):
+for i in range(1, 4): # 1 2 3
     print(i)
 
+for i in range(4): # 0 1 2 3
+    print(i)
+
+n = 5
+for i in range(n-1, -1, -1): # 4 3 2 1 0
+    print(i)
 # list1 = [0,1,2,3,4,5,6,7,8,9]
 # for i in range(len(list1)-1, -1, -1):
 #     print(f"{i}: {list1[i]}")
